03d1d47214c8b5bd0499cc5689fcb0151b246384
Penrix <73765460+Penrix@users.noreply.github.com>
2026-09-23 05:37:28 +0800

    WEB-M3-SEAM-001 export WebCodex seam module

---
+++ src/webcodex/index.ts
@@ -0,0 +1,13 @@
+export {
+  WEBCODEX_READ_FILES_ACTION_PATH,
+  WEBCODEX_READ_FILES_TOOL,
+  WebCodexHttpError,
+  invokeWebCodexReadFiles,
+  registerWebCodexReadFilesTool,
+} from './read-files.ts'
+export type {
+  WebCodexReadFilesArguments,
+  WebCodexReadFilesItem,
+  WebCodexReadFilesSeamOptions,
+  WebCodexToolResult,
+} from './read-files.ts'
